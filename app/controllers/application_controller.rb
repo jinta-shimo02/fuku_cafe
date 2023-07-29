c6ac60d4fb9312526d8e4e89ab152e9bbabdc8ab
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-07-29 09:54:01 +0900

    Add: ユーザー機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
