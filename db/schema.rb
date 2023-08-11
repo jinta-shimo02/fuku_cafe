efdc769034d53fb5665ecf7f30adca68bbac56bd
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-08-11 13:42:47 +0900

    Add: Shopテーブルにweb_siteカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_024334) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_042054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_024334) do
     t.string "place_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "web_site"
   end
 
   create_table "users", force: :cascade do |t|
