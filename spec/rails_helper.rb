27a572acf16593a38855c527f0d6360adf27dc4b
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-08-17 13:25:24 +0900

    Fix: 設定の修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include FActoryBot:Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
