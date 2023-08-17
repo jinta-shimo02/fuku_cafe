aae36758aeaa129ba5ac7ae18203d2c5ed0c53c1
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-08-17 22:35:38 +0900

    Add: テスト環境でcssをコンパイルしないよう設定（tailwindcssとsasscの依存関係）

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # css_compile
+  config.assets.css_compressor = nil
 end
