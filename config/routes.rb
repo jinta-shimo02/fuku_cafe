f49e8fbd36c233eb0aaf6660ceaca132ef02fbfc
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-08-30 23:48:23 +0900

    Fix: リンクの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'terms', to: 'static_pages#terms'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'home', to: 'maps#home'
+  match 'search', to: 'maps#search', via: [:get]
   devise_for :users
   resource :my_page, only: %i[show edit update]
-  match 'search', to: 'maps#search', via: [:get]
   resources :shops, only: %i[show]
 end
