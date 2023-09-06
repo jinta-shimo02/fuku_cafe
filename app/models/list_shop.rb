4557bd119b8a7a2e3e7278c48baf316f88026a83
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-09-06 23:34:48 +0900

    Add: テーブルの関連付けを追加

--- app/models/list_shop.rb
+++ app/models/list_shop.rb
@@ -1,2 +1,4 @@
 class ListShop < ApplicationRecord
+  belongs_to :shop_saved_list
+  has_many :shops
 end
--- app/models/shop_saved_list.rb
+++ app/models/shop_saved_list.rb
@@ -1,2 +1,5 @@
 class ShopSavedList < ApplicationRecord
+  belongs_to :user
+  has_many :list_shops, dependent: :destroy
+  has_many :shops, through: :list_shops
 end
