3cbbd775ab7557d410bd9b973855a103003d5cc7
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2023-10-22 23:08:54 +0900

    Fix: レビュー用のモーダル外をクリックするとモーダルが閉じるよう修正

--- app/javascript/controllers/review_modal_controller.js
+++ app/javascript/controllers/review_modal_controller.js
@@ -2,13 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="review-modal"
 export default class extends Controller {
-  static targets = ["reviewModal"]
+  static targets = ["reviewModal", "backGround"]
   connect() {
   }
 
   close(event) {
     if (event.detail.success) {
-      this.reviewModalTarget.classList.add("hidden");
+      this.backGroundTarget.classList.add("hidden");
+    }
+  }
+
+  closeModal() {
+    this.backGroundTarget.classList.add("hidden");
+  }
+
+  closeBackground(event) {
+    if(event.target === this.backGroundTarget) {
+      this.closeModal();
     }
   }
 }
